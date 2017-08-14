62d3b3d97b322bd2a827355cad5f579cbc81eb8b
Danielle Palmer <daniellezpalmer@gmail.com>
2017-08-14 15:09:36 -0500

    Finished all questions

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 console.log('hi');
 
-moment().day("Tuesday")
+let moment = require('moment');
+let chalk = require('chalk');
+
+console.log("It is " + chalk.blue(moment().format("dddd, MMMM Do YYYY, h:mm:ss a")) + ".");
+
+console.log("It is the " + chalk.magenta(moment().format("DDDo")) + " day of the year.");
+
+console.log("It is " + chalk.cyan((moment().get('hour') * 60 * 60 + moment().get('minute') * 60 + moment().get('second'))) + " seconds into the day.");
+
+let dLS;
+if (moment().isDST() === true)
+dLS = "is";
+else dLS = "isn\'t";
+
+console.log("It " + chalk.green(dLS) + " during Daylight Savings Time.");
+
+let leapYear;
+
+ if (moment().isLeapYear() === true)
+ leapYear = "is";
+ else leapYear = "isn\'t";
+
+console.log("It " + chalk.red(leapYear) + " a leap year.")
